c72314d5137c819fcb0567e5823e5714d18d57bd
Nabin Katwal <91274113+nabinkatwal7@users.noreply.github.com>
2024-09-24 18:29:03 +0545

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <h3 className="capitalize"> hello k xa timor khabar dfdsfsdf dsf</h3>
+    </div>
+  );
+}
